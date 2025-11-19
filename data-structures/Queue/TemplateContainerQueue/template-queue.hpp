e21fdc7c0b24ed59bca401d828000c98d02441ff
Nikola Georgiev <n.s.georgiev16@gmail.com>
2025-11-19 19:04:03 +0200

    Fix template-queue.hpp

--- data-structures/Queue/TemplateContainerQueue/template-queue.hpp
+++ data-structures/Queue/TemplateContainerQueue/template-queue.hpp
@@ -15,50 +15,17 @@ template <typename T, typename Container = std::deque<T>>
 class Queue
 {
 public:
-    void push(const T& value)
-    {
-        container.push_back(value);
-    }
+    void push(const T& value) { container.push_back(value); }
+    void push(T&& value) { container.push_back(std::move(value)); }
+    void pop() { container.pop_front(); }
 
-    void push(T&& value)
-    {
-        container.push_back(std::move(value));
-    }
+    T& front() { return container.front(); }
+    const T& front() const { return container.front(); }
+    T& back() { return container.back(); }
+    const T& back() const { return container.back(); }
 
-    void pop()
-    {
-        container.pop_front();
-    }
-
-    T& front()
-    {
-        return container.front();
-    }
-
-    const T& front() const
-    {
-        return container.front();
-    }
-
-    T& back()
-    {
-        return container.back();
-    }
-
-    const T& back() const
-    {
-        return container.back();
-    }
-
-    bool empty() const
-    {
-        return container.empty();
-    }
-
-    std::size_t size() const
-    {
-        return container.size();
-    }
+    bool empty() const { return container.empty(); }
+    std::size_t size() const { return container.size(); }
 private:
     Container container;
 };
